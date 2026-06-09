92db8f09e989b5eb4f3c0614df8e1eaa67291033
Jomon Joy <275461787+itsjomon@users.noreply.github.com>
2026-06-09 16:39:24 +0530

    Update Ans4.java

--- strings/practice/Ans4.java
+++ strings/practice/Ans4.java
@@ -28,3 +28,47 @@ public class Ans4 {
         }
     }
 }
+
+// Valid Anagram (LeetCode 242)
+// https://leetcode.com/problems/valid-anagram/description/
+
+// Solution 1: Sorting - O(n log n) time, O(n) space
+/*
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        if (s.length() != t.length()) return false;
+        char[] arr1 = s.toCharArray();
+        char[] arr2 = t.toCharArray();
+        Arrays.sort(arr1);
+        Arrays.sort(arr2);
+        return Arrays.equals(arr1, arr2);
+    }
+}
+*/
+
+// Solution 2: Counting Array - O(n) time, O(1) space
+/*
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        int[] count = new int[26];
+        int n1 = s.length();
+        int n2 = t.length();
+        if (n1 != n2) {
+            return false;
+        }
+
+        for (int i = 0; i < n1; i++) {
+            count[s.charAt(i) - 'a']++;
+            count[t.charAt(i) - 'a']--;
+        }
+
+        for(int i = 0; i < 26; i++) {
+            if (count[i] != 0) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
+*/
